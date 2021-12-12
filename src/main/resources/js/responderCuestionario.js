5f5549961474f3f47812b65451eab10feb018ad2
JairVqz <jair10.vasquez11@gmail.com>
2021-12-11 18:21:46 -0600

    Rama Video Prueba
    Co-authored-by: PabloLUC9832 <PabloLUC9832@users.noreply.github.com>

--- src/main/resources/js/responderCuestionario.js
+++ src/main/resources/js/responderCuestionario.js
@@ -1,13 +1,18 @@
 var btnEnviarCuestionario = document.getElementById("btnEnviarCuestionario");
 var btnVer = document.getElementById("btnVer");
+/*var grabar = document.getElementById("grabar")
+grabar.addEventListener("click", GRABAR)
+var detener = document.getElementById("detener")
+detener.addEventListener("click", DETENER)*/
 
-/*var chunks = [];
+
+var chunks = [];
 var mediaRecorder;
 
 navigator.mediaDevices.getUserMedia({
   audio: true, video: true
 }).then(function (x) {
-  // usar el flujo de datos 
+  /* usar el flujo de datos */
   console.log(x)
   mediaRecorder = new MediaRecorder(x);
 
@@ -67,35 +72,35 @@ navigator.mediaDevices.getUserMedia({
   }
 
   }).catch(function (err) {
-    // manejar el error 
+    /* manejar el error */
     console.log(err)
   });
 
-function GRABAR() {
-  mediaRecorder.start();
-  console.log(mediaRecorder.state);
-  console.log("recorder started");
-  // record.style.background = "red";
-  // record.style.color = "black";
-}
-
-function DETENER() {
-  mediaRecorder.stop();
-  console.log(mediaRecorder.state);
-  console.log("recorder stopped");
-  // record.style.background = "";
-  // record.style.color = "";
-}
-
-function enviar(stream) {
-  var formData = new FormData();
-  formData.append("videoGrabado", stream)
-  axios.post("http://localhost:4567/", formData, {
-    headers : {
-      "Content-Type" : "multipart/form-data"
-    }
-  })
-}*/
+  function GRABAR(params) {
+    mediaRecorder.start();
+    console.log(mediaRecorder.state);
+    console.log("recorder started");
+    // record.style.background = "red";
+    // record.style.color = "black";
+  }
+  
+  function DETENER(params) {
+    mediaRecorder.stop();
+    console.log(mediaRecorder.state);
+    console.log("recorder stopped");
+    // record.style.background = "";
+    // record.style.color = "";
+  }
+  
+  function enviar(stream) {
+    var formData = new FormData();
+    formData.append("videoGrabado", stream)
+    axios.post("http://localhost:4567/", formData, {
+      headers : {
+        "Content-Type" : "multipart/form-data"
+      }
+    })
+  }
 
 btnVer.addEventListener("click", function () {
     axios.get("http://localhost:4567/listaPreguntas")
@@ -126,107 +131,9 @@ btnVer.addEventListener("click", function () {
                 btnGrabar.textContent = "Grabar respuesta";
                 btnDetener.textContent = "Detener grabación";
 
-
-                var chunks = [];
-var mediaRecorder;
-var blob;
-var audioURL;
-
-navigator.mediaDevices.getUserMedia({
-  audio: true, video: true
-}).then(function (x) {
-  // usar el flujo de datos 
-  console.log(x)
-  mediaRecorder = new MediaRecorder(x);
-
-  var camara = document.getElementById("camara")
-  camara.srcObject = x
-  camara.onloadedmetadata = function (e) {
-    // Do something with the video here.
-    camara.play()
-  };
-  console.log(camara)
-
-  mediaRecorder.onstop = function (e) {
-    console.log("data available after MediaRecorder.stop() called.");
-
-    var clipName = prompt('Enter a name for your sound clip');
-
-    var clipContainer = document.createElement('article');
-    var clipLabel = document.createElement('p');
-    var audio = document.createElement('video');
-    audio.width="150"
-    var deleteButton = document.createElement('button');
-    // generar una liga
-    var a = document.createElement('a')
-    var texto = document.createTextNode("descarga")
-    a.appendChild(texto)
-
-    clipContainer.classList.add('clip');
-    audio.setAttribute('controls', '');
-    deleteButton.innerHTML = "Delete";
-    clipLabel.innerHTML = clipName;
-
-    var soundClips = document.getElementById("xxx")
-    clipContainer.appendChild(audio);
-    clipContainer.appendChild(clipLabel);
-    clipContainer.appendChild(deleteButton);
-    clipContainer.appendChild(a)
-    soundClips.appendChild(clipContainer);
-
-    audio.controls = true;
-    //var blob = new Blob(chunks, { 'type': 'video/webm; codecs=vp8' });
-    blob = new Blob(chunks, { 'type': 'video/webm; codecs=vp8' });
-    chunks = [];
-    var audioURL = URL.createObjectURL(blob);
-    audio.src = audioURL;
-    console.log("recorder stopped");
-    a.href = audioURL
-    a.download = "video.mp4"
-
-    deleteButton.onclick = function(e) {
-      evtTgt = e.target;
-      evtTgt.parentNode.parentNode.removeChild(evtTgt.parentNode);
-    }
-  }
-
-  mediaRecorder.ondataavailable = function(e) {
-    chunks.push(e.data);
-    enviar(e.data)
-  }
-
-  }).catch(function (err) {
-    // manejar el error 
-    console.log(err)
-  });
-
-function GRABAR() {
-  mediaRecorder.start();
-  console.log(mediaRecorder.state);
-  console.log("recorder started");
-  // record.style.background = "red";
-  // record.style.color = "black";
-}
-
-function DETENER() {
-  mediaRecorder.stop();
-  console.log(mediaRecorder.state);
-  console.log("recorder stopped");
-  // record.style.background = "";
-  // record.style.color = "";
-}
-
-function enviar(stream) {
-  var formData = new FormData();
-  formData.append("videoGrabado", stream)
-  axios.post("http://localhost:4567/", formData, {
-    headers : {
-      "Content-Type" : "multipart/form-data"
-    }
-  })
-}
-
                 btnEnviar.textContent = "Enviar respuesta";
+
+
                 btnEnviar.addEventListener("click",() => {
                     console.log(id.innerText)
                     axios.post("http://localhost:4567/guardarRespuesta",{
